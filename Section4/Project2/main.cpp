c9a8c46376824cf367b593af7c71089b95bbbbf2
Quan Sun <quans1991@gmail.com>
2023-06-01 18:57:45 +0800

    section 5

--- Section4/Project2/main.cpp
+++ Section4/Project2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// This is a comment
 int main() {
     int favorite_number;
     cout << "What is your favorite number (0 - 100): " ;
@@ -7,3 +8,8 @@ int main() {
     cout << "Amazing! "<< favorite_number << " is also my favorite number!" << endl;
     return 0;
     }
+    
+/* 
+This is multiple line 
+comments 
+ */
---
+++ Section5/BasicIO/main.cpp
@@ -0,0 +1,35 @@
+// Basic I/O using cin and cout
+#include <iostream>
+using namespace std;
+int main() {
+    // cout<< "Default project" << endl;
+//    cout<< "Default project";
+//    cout << "Hello";
+//    cout << "World" << endl;
+//    cout << "Hello\nworld\n!\n";
+    int num1;
+    int num2;
+    double num3;
+    
+//    cout << "Enter an integer: ";
+//    cin >> num1;
+//    cout << "You entered: " << num1 << endl;
+//    cout << "Enter your first integer: ";
+//    cin >> num1;
+//    cout << "Enter your second integer: ";
+//    cin >> num2;
+//    cout << "You entered " << num1 << " and " << num2 <<endl;
+//
+//    cout << "Enter two integer separated with a space: ";
+//    cin >> num1 >> num2;
+//    cout <<"You entered " << num1 << " and " << num2 <<endl;
+//
+//    cout << "Enter a double: ";
+//    cin >> num3;
+//    cout << "You entered: " << num3 <<endl;
+ 
+    cout << "Enter a integer and a doubel: ";
+    cin >> num1 >> num3; // try enter 10.5
+    cout << "The numbers you entered are " << num1 << " and " << num3<<endl;
+   return 0;
+    }
